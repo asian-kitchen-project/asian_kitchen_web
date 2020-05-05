78218d236fcdcf6bdbcf7269b26d8dabf5f633ac
suckerK <zuck.3rx@gmail.com>
2020-05-05 19:18:23 +0900

    info : 管理ページのレイアウト変更

--- delivery/admin.py
+++ delivery/admin.py
@@ -6,6 +6,10 @@ from delivery.models import (
     Category,
     Food
     )
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('category', 'name', 'price', 'is_sale')
+    list_editable = ('price', 'is_sale')
 
 admin.site.register(Category)
-admin.site.register(Food)
+
